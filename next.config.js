7c0dd989cbd87b25aa9a733be88e3ca736bb2242
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-18 12:11:44 +0530

    feat: next-ified page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config) {
+    config.module.rules[2].oneOf?.forEach((one) => {
+      if (!`${one.issuer?.and}`.includes("_app")) return;
+      one.issuer.and = [path.resolve(__dirname)];
+    });
+    return config;
+  },
+  sassOptions: {
+    additionalData: `@import "styles/root/_mixins.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/Components/BottomSheet/BottomSheet.js
@@ -0,0 +1,67 @@
+import React from "react";
+import GInput from "../GInput/GInput";
+import "./BottomSheet.scss";
+
+export default function BottomSheet({ setData, setIsOpen }) {
+  const [formData, setFormData] = React.useState({
+    name: "",
+    email: "",
+    majorAndYear: "1",
+    answer: "",
+  });
+  return (
+    <div className="BottomSheetWrapper">
+      <form
+        className="BottomSheetWrapper__form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setData(formData);
+          setIsOpen(false);
+        }}
+      >
+        <div className="BottomSheetWrapper__form--top">
+          <img src="/assets/gdscLogo.png" />
+          {/* <Image src="/gdscLogo.png" width={200} height={200} /> */}
+          <h3>Register</h3>
+          <p>Use your Shiv Nadar University Account</p>
+        </div>
+        <div className="BottomSheetWrapper__form--inputs">
+          <GInput
+            id="name"
+            label="Name"
+            setValue={(e) => {
+              setFormData({ ...formData, name: e.target.value });
+            }}
+          />
+          <GInput
+            id="snuId"
+            type="email"
+            label="SNU Net Id"
+            pattern="[a-z0-9._%+-]+@snu.edu.in$"
+            setValue={(e) => {
+              setFormData({ ...formData, email: e.target.value });
+            }}
+          />
+          <select
+            onChange={(e) =>
+              setFormData({ ...formData, majorAndYear: e.target.value })
+            }
+          >
+            <option value="1">1st Year</option>
+            <option value="2">2nd Year</option>
+            <option value="3">3rd Year</option>
+            <option value="4">4th Year</option>
+          </select>
+        </div>
+        <p className="BottomSheetWrapper__form--terms">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et. dolor
+          sit
+          <a href="#">Learn more</a>
+        </p>
+        <div className="BottomSheetWrapper__form--buttonContainer">
+          <button type="submit">Next</button>
+        </div>
+      </form>
+    </div>
+  );
+}

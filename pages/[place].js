1a47dc1acb2949458cbff0c101815b7a556c99a5
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-22 19:54:30 +0530

    feat: Random place facts and questions

--- pages/[place].js
+++ pages/[place].js
@@ -6,10 +6,140 @@ import GInput from "../src/Components/GInput/GInput";
 import Spinner from "../src/Components/Spinner/Spinner";
 import "../styles/routes/Place.scss";
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  const questions = [
+    "Something that catches more attention when it is louder.",
+    "What gets larger the more you take away from it?",
+    "What's something that belongs to you but others tend to use it more than you?",
+  ];
+
+  const funFacts = {
+    "a-block": {
+      location: "A block",
+      facts: ["Morning nightmare of boys."],
+    },
+    "c-block": {
+      location: "C block",
+      facts: ["I am the luckiest block I have food stalls on either sides."],
+    },
+    "d-block": {
+      location: "D block",
+      facts: [
+        "Amazon packets are waiting for their owners!!!",
+        "I am usually most preferred block by the clubs.",
+      ],
+    },
+    library: {
+      location: "Library",
+      facts: [
+        "The place where non-ac people come and hang out.",
+        "You might be having a gala time here until the curator enters.",
+      ],
+    },
+    arcade: {
+      location: "Arcade",
+      facts: ["Grocery store with so called ADIDAS showroom :)"],
+    },
+    "mini-mart": {
+      location: "Mini Mart",
+      facts: [
+        "The place where you will always think “The other line moves faster.",
+      ],
+    },
+    dh1: {
+      location: "DH1",
+      facts: ["Trust me! I am overcrowded mostly during 4-5."],
+    },
+    dh2: {
+      location: "DH2",
+      facts: [
+        "Only SNU has No. 2 bigger than 3",
+        "The only dh where we get curd:)",
+      ],
+    },
+    dh3: {
+      location: "DH3",
+      facts: ["Cool atmosphere but its far better if you skip the meal."],
+    },
+    "hostel-1a": {
+      location: "Hostel 1A",
+      facts: ["I'm Hostel 1A"],
+    },
+    "hostel-1b": {
+      location: "Hostel 1B",
+      facts: [
+        "Whenever you enter the lift you have to knock twice.",
+        "Freshers getting freshing up for the college.",
+        "Jadoo's(Dog’s name) territory.",
+      ],
+    },
+    "hostel-2a": {
+      location: "Hostel 2A",
+      facts: ["I'm Hostel 2A"],
+    },
+    "hostel-2b": {
+      location: "Hostel 2B",
+      facts: ["I'm Hostel 2B"],
+    },
+    towers: {
+      location: "Towers",
+      facts: [
+        "Girls judge each other for their dresses and makeup.",
+        "Too lazy to go to the arcade?? Let's go to the tuck shops.",
+      ],
+    },
+    isc: {
+      location: "ISC",
+      facts: [
+        "The best place to enjoy the evenings play gossip enjoy eat and cheer!!",
+        "Booking gym slot? or sleep?",
+      ],
+    },
+    "snu-lake": {
+      location: "SNU Lake",
+      facts: [
+        "One can view some amazing flora and fauna along with perfect sunrise and sunset.",
+        "Everything except a lake:(",
+      ],
+    },
+    dargah: {
+      location: "DARGAH",
+      facts: [
+        "The place to find uncle, John.",
+        "Students of different religions go and pray before the end of the semester.😂😂",
+        "Mandatory spot to visit on a Saturday night out (but we aware of neel gaya 😉)",
+      ],
+    },
+    "horse-stables": {
+      location: "Horse Stables",
+      facts: [
+        "The place you can find the another lead of the famous Disney “secretariat” movie.",
+      ],
+    },
+    "c-and-d": {
+      location: "C&D",
+      facts: [
+        "I am suddenly loved even more when mess says “Lauki ki sabji in menu” today.",
+        "The best place to chill with friends and sometimes professors too😂",
+      ],
+    },
+  };
+  return {
+    props: {
+      question: questions[Math.floor(Math.random() * questions.length)],
+      funFacts: funFacts,
+      factIndex: Math.floor(
+        Math.random() * funFacts[context.params["place"]].facts.length
+      ),
+      place: context.params["place"],
+    },
+  };
+}
+
+export default function Home({ question, funFacts, factIndex, place }) {
   const [isOpen, setIsOpen] = useState(true);
   const router = useRouter();
-  const place = router.query.place;
+  // const place = router.query.place;
   const [isDesktopSize, setIsDesktopSize] = useState(false);
   const [loadingScreen, setLoadingScreen] = useState(false);
   const [localData, setLocalData] = useState({
@@ -36,11 +166,13 @@ export default function Home() {
   useEffect(() => {
     if (localStorage.getItem("data")) {
       setLocalData({
-        location: place,
+        question: question,
+        location: funFacts[place].location,
         ...JSON.parse(localStorage.getItem("data")),
       });
     }
   }, [isOpen]);
+
   return (
     <>
       {loadingScreen && (
@@ -48,7 +180,7 @@ export default function Home() {
           <Spinner />
         </div>
       )}
-      {!isDesktopSize && (
+      {!isDesktopSize && Object.keys(funFacts).includes(place) && (
         <div className="PlaceWrapper">
           <Head>
             <title>GDSC</title>
@@ -79,17 +211,14 @@ export default function Home() {
 
               <div className="PlaceWrapper__funfact">
                 <h3>
-                  Welcome to <span>{place}</span>
+                  Welcome to <span>{funFacts[place].location}</span>
                 </h3>
                 <div className="PlaceWrapper__funfact--card">
                   <img
                     className="PlaceWrapper__funfact--placeImg"
                     src="/assets/DiningGif.png"
                   />
-                  <p>
-                    The dining hall with the most number of attractions for a
-                    foodie with evening snacks to midnight snacks.
-                  </p>
+                  <p>{funFacts[place].facts[factIndex]}</p>
                   <img
                     className="PlaceWrapper__funfact--noise"
                     src="/assets/Noise.svg"
@@ -104,31 +233,44 @@ export default function Home() {
                   setData(localData);
                   setLoadingScreen(true);
 
-                  fetch("/api/excel", {
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(localData),
-                  })
-                    .then((r) => r.text())
-                    .then((result) => {
-                      setLoadingScreen(false);
-                      if (
-                        window.confirm("Thanks for answering the question!")
-                      ) {
-                        window.location.href = post;
-                      }
+                  if (localStorage.getItem("isFilled") === null) {
+                    fetch("/api/excel", {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify(localData),
                     })
-                    .catch(() => {
-                      window.alert("Some error occured. Please try again.");
-                    });
+                      .then((r) => r.text())
+                      .then((result) => {
+                        setLoadingScreen(false);
+                        localStorage.setItem("isFilled", "true");
+                        if (
+                          window.confirm("Thanks for answering the question!")
+                        ) {
+                          window.location.href = post;
+                        }
+                      })
+                      .catch(() => {
+                        window.alert("Some error occured. Please try again.");
+                      });
+                  } else {
+                    if (
+                      window.confirm(
+                        "Looks right you have already filled it once. You can follow us on instagram though(if you haven't yet)"
+                      )
+                    ) {
+                      window.location.href = post;
+                    }
+                  }
                 }}
               >
-                <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+                <h2>
+                  Answer this question and get a chance to be featured during
+                  GDSC orientation
+                </h2>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et
-                  massa mi. Aliquam in hendrerit urna. Pellentesque sit?
+                  <i>{question}</i>
                 </p>
                 <GInput
                   id="answer"
@@ -154,7 +296,7 @@ export default function Home() {
           )}
         </div>
       )}
-      {isDesktopSize && (
+      {(isDesktopSize || !Object.keys(funFacts).includes(place)) && (
         <div className="DesktopOverLay">
           <span>
             <img className="DesktopOverLay__logo" src="/assets/gdscLogo.png" />
@@ -164,7 +306,11 @@ export default function Home() {
             className="DesktopOverLay__lostImg"
             src="/assets/desktopOverlay.png"
           />
-          <p>uhh you’re not supposed to be accessing this in a desktop</p>
+          <p>
+            {!Object.keys(funFacts).includes(place)
+              ? "This place is not in SNU!"
+              : "uhh you're not supposed to be accessing this in a desktop"}
+          </p>
         </div>
       )}
     </>
